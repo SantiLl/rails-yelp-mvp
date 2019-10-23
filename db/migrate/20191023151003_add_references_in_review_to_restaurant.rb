2067a982e6d69803de33bc1d6ed3f075adfcf337
Santiago Llapur <santillapur@gmail.com>
2019-10-23 19:21:08 -0300

    exercise 2 solved, linked 2 mvc and work them together

---
+++ db/migrate/20191023151003_add_references_in_review_to_restaurant.rb
@@ -0,0 +1,5 @@
+class AddReferencesInReviewToRestaurant < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :reviews, :restaurant, index: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+Restaurant.create(name: "Bellagamba", address: "Malabia 123", category: "italian")
+Restaurant.create(name: "Tipo 7", address: "Niceto 123", category: "belgian")
+Restaurant.create(name: "Croxi", address: "Conde 123", category: "french")
+Restaurant.create(name: "Mc Donals", address: "Elcano 123", category: "japanese", phone_number: "1234")
+Restaurant.create(name: "Kansas", address: "Libertador 123", category: "chinese", phone_number: "5678")
+
+puts 'Finished!'
